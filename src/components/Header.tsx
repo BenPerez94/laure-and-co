2b0cc396cdcab77332ed047dc38d9345bbc5c5f6
Ben <benjamin.perez@outlook.fr>
2025-07-01 17:49:42 +0200

    bug correct

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { Mail, Phone } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
